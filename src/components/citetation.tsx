da6a6bfcc1db14412c0667358ed2c8f1c0d20065
Levi Deang <arron.levi1@gmail.com>
2023-01-13 19:20:25 +0800

    Added link to open weather

--- src/components/citetation.tsx
+++ src/components/citetation.tsx
@@ -7,7 +7,13 @@ export const Citetation = () => {
       className="z-10 m-2 mt-12 text-center text-sm capitalize dark:font-semibold dark:text-gray-300"
     >
       <span className="mb-2 block">Powered By:</span>
-      <img src={ow} alt="Open Weather" width={100} height={50} />
+      <a
+        href="https://openweathermap.org/"
+        target="_blank"
+        aria-label="link to open weather"
+      >
+        <img src={ow} alt="Open Weather" width={100} height={50} />
+      </a>
     </div>
   );
 };
